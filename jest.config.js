3c729e9b8a2088ea4150c2e4b63bc365fbbb9df6
Luiz LuLz <luiz.cfj@apilab.ufsc.br>
2025-10-06 16:32:08 -0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-
   // Indicates which provider should be used to instrument code for coverage. 'v8' is modern and efficient.
   coverageProvider: "v8",
-
   // The root directory that Jest should scan for tests and modules within
   testMatch: [
     "<rootDir>/test/**/*.js"
